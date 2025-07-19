86347b7f022a99454ca0ee18335890499c94a6d3
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2025-07-19 23:31:54 +0200

    height map example

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# marching_stomachs
+# marching\_stomachs
+
+## Getting started
+
+Install correct zig version.
+
+```
+wget https://ziglang.org/download/0.14.1/zig-x86_64-linux-0.14.1.tar.xz
+tar -xf zig-x86_64-linux-0.14.1.tar.xz
+# add it to your path
+```
+
+Install correct zig lsp version.
+
+```
+wget https://builds.zigtools.org/zls-linux-x86_64-0.14.0.tar.xz
+tar -xf builds.zigtools.org/zls-linux-x86_64-0.14.0.tar.xz
+```
+
+Finally run, or build the project.
+
+```
+zig build
+
+zig build run
+```
--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,73 @@
-// raylib-zig (c) Nikolas Wipper 2023
-
 const rl = @import("raylib");
 
 pub fn main() anyerror!void {
     // Initialization
-    //--------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "An army marches on it's stomach");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+
+    // Setup camera
+
+    const cameraPosition: rl.Vector3 = .{ .x = 18, .y = 21, .z = 18 };
+    const cameraTarget: rl.Vector3 = .{ .x = 0, .y = 0, .z = 0 };
+    const cameraUp: rl.Vector3 = .{ .x = 0, .y = 1, .z = 0 };
+    const cameraProjection = rl.CameraProjection.perspective;
+    var camera = rl.Camera{ .fovy = 45.0, .position = cameraPosition, .up = cameraUp, .projection = cameraProjection, .target = cameraTarget };
+
+    // Setup terrain texture
+    const image: rl.Image = try rl.loadImage("resources/heightmap.png");
+
+    const texture: rl.Texture2D = try rl.loadTextureFromImage(image);
+    defer rl.unloadTexture(texture);
+
+    const meshSize = rl.Vector3{ .x = 16, .y = 8, .z = 16 };
+    const mesh = rl.genMeshHeightmap(image, meshSize);
+    defer rl.unloadMesh(mesh);
+
+    var model = try rl.loadModelFromMesh(mesh);
+    model.materials[0].maps[@intFromEnum(rl.MATERIAL_MAP_DIFFUSE)].texture = texture;
+
+    const mapPosition = rl.Vector3{ .x = -8.0, .y = 0.0, .z = -8.0 };
+
+    rl.unloadImage(image);
+
+    //-------------------------------------------------------------------------
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
-        //----------------------------------------------------------------------------------
+        //---------------------------------------------------------------------
         // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        //---------------------------------------------------------------------
+
+        rl.updateCamera(&camera, .first_person);
 
         // Draw
-        //----------------------------------------------------------------------------------
+        //---------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
-        //----------------------------------------------------------------------------------
+        // Draw 3D
+
+        rl.beginMode3D(camera);
+
+        rl.drawModel(model, mapPosition, 1, .red);
+        rl.drawGrid(20, 1.0);
+
+        rl.endMode3D();
+
+        rl.drawTexture(texture, screenWidth - texture.width - 20, 20, .white);
+        rl.drawRectangleLines(screenWidth - texture.width - 20, 20, texture.width, texture.height, .green);
+        rl.drawFPS(10, 10);
+
+        rl.drawText("Marching Stomachs", 190, 200, 20, .light_gray);
+        //---------------------------------------------------------------------
     }
 }
